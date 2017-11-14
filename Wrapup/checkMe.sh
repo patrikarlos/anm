d7b2ae7c0485ec47a5d145bd722c2311368cfbe7
Patrik Arlos <PatrikArlos@bth.se>
2017-11-14 10:20:15 +0100

    Added Wrapup lab test.

---
+++ Wrapup/checkMe.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+
+myBasedir=$(pwd)
+
+myRootDir="$1"
+feedbackFile="$2"
+logType="$3"
+
+
+if [ -z "$logType" ]; then 
+    logType=1
+fi
+
+
+
+
+logIt() {
+    theStuff="$1"
+    logstr=$(echo $(date +"%F %T") "\t(Root) $theStuff")
+    if (( $logType==3 )); then 
+	## File and stdout
+	echo -e "$logstr " | tee -a $feedbackFile
+    elif (( $logType==2 )); then 
+	## File only
+	echo -e "$logstr " >> $feedbackFile
+    elif (( $logType==1 )); then 
+	## STDOUT only
+	echo -e "$logstr "
+    elif (( $logType==0 )); then 
+	## Silent
+	echo "."
+    else
+	echo "Unknown logType ($logType). Supports 0-3"
+    fi
+}
+
+checkFile(){
+    theFile="$1"
+#    logIt "Checking $theFile"
+    if [[ -f "$theFile" ]]; then
+	logIt "Checking $theFile, OK"
+    else
+	logIt "Checking $theFile, Missing"
+    fi
+}
+
+checkFileOpt(){
+    theFile="$1"
+#    logIt "Checking $theFile"
+    if [[ -f "$theFile" ]]; then
+	logIt "Checking $theFile (optional), OK"
+    fi
+}
+
+checkFileMode(){
+    theFile="$1"
+    if [[ -f "$theFile" ]]; then
+#	logIt "Checking $theFile"
+	if [[ -x "$theFile" ]]; then
+	    logIt "$theFile it's executable"
+	else
+	    logIt "$theFile is missing the executable bit and will not start (man chmod)."
+	fi
+    fi
+}
+
+
+echo $(date +"%F %T") "Log.info myRootDir=$myRootDir" 
+echo $(date +"%F %T") "feedbackFile='$feedbackFile'"
+echo $(date +"%F %T") "logType='$logType' "
+
+cd "$myRootDir"
+#cd "$classid-$userid"
+currdir=$(pwd) 
+logIt "We will check the files in $currdir"
+logIt "Checking files in the root dir (readme.txt db.conf) "
+readmeCnt=$(ls | grep -i readme.txt | wc -l )
+if (( $readmeCnt >= 1 )) ; then
+    lenreadme=$(ls -la | grep -i readme.txt | head -1 | awk '{print $5}')
+    logIt "readme.txt present and has $lenreadme characters"
+else
+	logIt "In the folder of your submitted archive, the readme.txt file was missing."
+fi
+
+logIt "Checking main db.conf, in " $(pwd) 
+dbconfCnt=$(ls | grep -i db.conf | wc -l )
+if (( $dbconfCnt >= 1 )) ; then
+    dbconflen=$(ls -la | grep -i db.conf | head -1 | awk '{print $5}')
+    logIt "db.conf present and has $dbconflen characters"
+    logIt "looking for variables in db.conf"
+    hvar=$(grep -i host db.conf | wc -l)
+    portvar=$(grep -i port db.conf | wc -l)
+    dbvar=$(grep -i database db.conf | wc -l)
+    unvar=$(grep -i username db.conf | wc -l)
+    pwvar=$(grep -i password db.conf | wc -l)
+    if (( $hvar &&  $portvar && $dbvar  && $unvar &&  $pwvar )); then
+	logIt "All variables seems present"
+	leftLines=$(grep -v -i port db.conf | grep -v -i database | grep -v -i host | grep -v -i username | grep -v -i password | wc -l )
+	logIt "There are $leftLines that did not match the parameters."
+	logIt "Replacing with my db.conf"
+	logIt "(backup)db.conf db_orginal.conf  "
+	cp db.conf db_orginal.conf 
+	logIt "(copy) ($myBasedir/db.conf -> db.conf) : "
+	cp $myBasedir/db.conf db.conf 
+	logIt "Difference between db_original and the used". 
+	slurp=$(echo " "; diff -y db.conf db_orginal.conf)
+	logIt "$slurp"
+	
+    else
+	logIt "Missing some variables";
+	missVar=""
+	for variable in host port database username password
+	do
+	    result=$(grep $variable assignment1/db.conf | wc -l)
+	    if (( $result==0)); then
+		#		    logIt "$variable "
+		missVar="$missVar $variable"
+	    fi
+	done
+	logIt "Main: Missing the following variables in db.conf ; $missVar "
+    fi
+else
+    logIt "db.conf is missing"
+    logIt "In the folder of your submitted archive, the readme.txt file was missing."
+fi
+
+
+logIt "Checking Assignments (assignment1, assignment2 and assignment3"
+assCount=0
+missA=""
+for assin in assignment1 assignment2 assignment3
+do 
+    if [ -d "$assin" ]; then
+	if [ ! -f $assin/readme.txt ]; then
+	    logIt "In the folder of your submitted archive, the readme.txt for $assin missing "
+	fi
+	assCount=$[$assCount + 1]
+    else
+	logIt "In the folder of your submitted archive, the $assin folder is missing"
+	missA="$missA $assin"
+    fi
+done
+
+slp=$(echo "Got $assCount assignment" in $(pwd))
+logIt "$slp"
+if (( $assCount < 3 )); then
+    logIt "ERROR: You submission misses some assignments ($missA), please update."
+    exit;
+else 
+    slp=$(echo "Checking assignments in " $(pwd))
+    logIt "$slp"
+    
+    ## Assignment 1 starts 
+    logIt "Assignment1"
+    logIt "Checking report.pdf"
+    a2=$(ls -ls assignment1/report.pdf 2>/dev/null | wc -l)
+    if (( $a2>=1 )); then
+	logIt "One or more PDF files present. ";
+    else
+	logIt "Missing report (pdf)"
+	logIt "Assignment 1: Missing report (as a pdf file)"
+    fi
+    
+    checkFile "assignment1/readme.txt" 
+    checkFile "assignment1/mrtgconf"
+    checkFileMode "assignment1/mrtgconf"
+    checkFile "assignment1/backend"  
+    checkFileMode "assignment1/backend"  
+    checkFile "assignment1/backend.sh" 
+    checkFileMode "assignment1/backend.sh" 
+    checkFile "assignment1/index.*" 
+    checkFileOpt "assignment1/Makefile" 
+    
+    
+
+
+    
+    ## Assignment 2 starts
+    logIt "Assignment 2"
+    checkFile "assignment2/readme.txt"
+    checkFileOpt "assignment2/Makefile"
+    checkFile "assignment2/backend"
+    checkFileMode "assignment2/backend"
+    checkFile "assignment2/backend.sh"
+    checkFileMode "assignment2/backend.sh"
+    checkFile "assignment2/index.*"
+    
+    
+    ## Assignment 3 starts
+    logIt "Assignment 3"
+    checkFile "assignment3/readme.txt"
+    checkFileOpt "assignment3/Makefile"
+    checkFile "assignment3/trapDeamon"
+    checkFile "assignment3/snmptrapd.conf"
+    checkFile "assignment3/index.*"
+   
+    ## Assignments done
+    
+fi
+
+
---
+++ evaluateLabs_wrapup.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if [ "$1" == "-h" ]; then
+    echo "Evaluate ANM labs.";
+    echo "Usage:"
+    echo "$1 <labfolder>"
+    echo "Labfolder contains the downloaded folder from Its learning."
+    exit
+fi
+
+myBasedir=$(pwd)
+logType=1
+#timestr=$(date +%Y-%m-%d_%H%M)
+#feedbackFile="$myBasedir/Feedback".$timestr."log"
+myRootDir=$1
+
+source Wrapup/supportfunc
+
+#logIt "Feedback will be in $feedbackFile"
+
+
+
+for student in $myRootDir/*
+do 
+    logIt "Checking $student in $myRootDir"
+
+    ##Find the archive
+    archives=$(ls "$student"/*.tar.gz | wc -l )
+    if [ "$archives" != "1" ]; then
+	logIt "Missing archive, or too many"  
+	logIt "Missing archive (tar.gz) or too many files submitted"
+	logIt "You should submit one archive (<coursecode>-<userid>.tar.gz, see lab assignment for details."
+	logIt "Your submission contained "
+	logIt $(ls -la *.tar.gz)
+	continue;
+    fi
+    archivename=$(ls "$student"/*.tar.gz)
+    logIt "One archive found, $archivename." 
+    userid=$(basename "$archivename" .tar.gz)
+    array=(${userid//-/ })
+    classid=${array[0]}
+    userid=${array[1]}
+    logIt "Class and UserId (based on archive name) => $classid and $userid" 
+#    tar -zxvf *.tar.gz 
+    bname=$(echo "$classid-$userid")
+    builddir=$(mktemp -t -d ANM-XXXX )
+    fullpath="$archivename"
+    logIt "Extracting Archive ($archivename) into $builddir ."
+    logIt "Going into $builddir ." 
+    cd $builddir
+    tar -zxvf "$fullpath" > unpack.log
+    fcount=$(ls | wc -l)
+    if (( $fcount > 2 )); then
+	logIt "Folder contains too many files ($fcount), should just have one folder and the unpack.log."
+	logIt "Generating feedback, then aborting this student."
+	## Generate feedback log
+	logIt "Your submitted archive did not contain the right information, it should just be a folder <coursecode>-<userid>, see lab assignment for details." 
+	subfiles=$(ls -la | grep -v 'unpack.log')
+	logIt "$subfiles"
+	cd ..	
+#	rm -rf $builddir
+	continue;
+    fi
+    logIt "Copying reference db.conf" 
+    cp -v "$myBasedir/Wrapup/db.conf" "$builddir/"
+    logIt "Entering user folder, $classid-$userid . "
+   
+    $myBasedir/Wrapup/checkMe.sh "$classid-$userid" "$builddir/mylog" 2
+    
+    logIt "Returned from test. "
+    sleep 1
+    
+    status=$(grep -i "Missing" "$builddir/mylog" | wc -l)
+    logIt "There were $status 'Missing' items."
+
+    status=$(grep -i "Error" "$builddir/mylog" | wc -l)
+    logIt "There were $status 'Error' items."
+
+    ## Assignments done
+    
+    logIt "Preparing $builddir that we can read it"
+    chmod -R ag+rwx $builddir
+done
